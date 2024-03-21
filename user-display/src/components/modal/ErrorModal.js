cf7bc92d46930111761f05de28679f4f1e2ef366
alfasierraconnects <alfasierraconnects@gmail.com>
2024-03-22 00:27:26 +0530

    used Portals

--- user-display/src/components/modal/ErrorModal.js
+++ user-display/src/components/modal/ErrorModal.js
@@ -1,4 +1,28 @@
 import React from "react";
+import ReactDOM from "react-dom";
+
+const Backdrop = (props) => {
+  return <div className="fixed inset-0 bg-black opacity-50 z-50"></div>;
+};
+
+const Overlay = (props) => {
+  return (
+    <div className="absolute inset-0 flex justify-center items-center z-50">
+      <div className="relative shadow-lg rounded-xl bg-white overflow-hidden w-96">
+        <h2 className="text-white bg-purple-950 text-xl font-bold p-4">
+          An error occurred!
+        </h2>
+        <p className="font-bold p-4 mb-20">{props.errorText}</p>
+        <button
+          onClick={props.resetError}
+          className="absolute bottom-0 right-0 text-white bg-purple-950 p-2 px-4 m-4 rounded hover:shadow-lg hover:shadow-purple-500/40 active:opacity-85 active:shadow-none"
+        >
+          Okay
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default function ErrorModal(props) {
   function errorText() {
@@ -8,19 +32,15 @@ export default function ErrorModal(props) {
   }
 
   return (
-    <div className="absolute inset-0 backdrop-blur-sm flex justify-center items-center">
-      <div className="relative shadow-lg rounded-xl bg-white overflow-hidden w-96 ">
-        <h2 className="text-white bg-purple-950 text-xl font-bold p-4">
-          An error occured!
-        </h2>
-        <p className="font-bold p-4 mb-20">{errorText()}</p>
-        <button
-          onClick={props.resetError}
-          className="absolute bottom-0 right-0 text-white bg-purple-950 p-2 px-4 m-4 rounded hover:shadow-lg hover:shadow-purple-500/40 active:opacity-85 active:shadow-none"
-        >
-          Okay
-        </button>
-      </div>
-    </div>
+    <>
+      {ReactDOM.createPortal(
+        <Backdrop />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <Overlay errorText={errorText()} resetError={props.resetError} />,
+        document.getElementById("overlay-root")
+      )}
+    </>
   );
 }
